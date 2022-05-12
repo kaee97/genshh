93a288ed5d0ad64cfff9006cec5bfb198ff5602e
Kaee97 <61938549+kaee97@users.noreply.github.com>
2022-05-12 14:32:33 +0700

    Add files via upload

---
+++ proxy_config.py
@@ -0,0 +1,16 @@
+import os
+
+USE_SSL = TRUE
+REMOTE_HOST = "hk1.genshinps.me"
+REMOTE_PORT = 443
+
+if os.getenv('MITM_REMOTE_HOST)!=None:
+  REMOTE_HOST = os.getenv('MITM_REMOTE_HOST')
+if os.getenv('MITM_REMOTE_PORT)!=None:
+  REMOTE_PORT = int(os.getenv('MITM_REMOTE_PORT'))
+if os.getenv('MITM_USE_SSL)!=None:
+  USE_SSL = bool(os.getenv('MITM_USE_SSL'))
+  
+print('MITM Remote Host:'+REMOTE_HOST)
+print('MITM Remote Port:'str(REMOTE_PORT))
+print('MITM Use SSL'+str(USE_SSL))
